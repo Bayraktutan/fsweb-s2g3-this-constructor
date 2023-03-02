42f02c3aeee5feead90a85b39310f14ae17b53f9
Oguz <oguzbayraktutan@hotmail.com>
2023-03-02 13:36:11 +0300

    Solved

--- index.js
+++ index.js
@@ -14,8 +14,21 @@
         + Bu metotla, `isim` ve `yas`i içeren bir string döndürmelisin. (Örn: "Mary, 50")
 */
 
-function Kisi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function Kisi(isim, yas) {
+  this.mide = [];
+  this.ye = function (birYemek) {
+    if (this.mide.length <= 10) {
+      this.mide.push(birYemek);
+    }
+  };
+  this.bosalt = function () {
+    this.mide = [];
+  };
+  this.isim = isim;
+  this.yas = yas;
+  this.toString = function () {
+    return `${this.isim}, ${this.yas}`;
+  };
 }
 
 /*
@@ -34,29 +47,83 @@ function Kisi(/* kodlar buraya */) {
         +  "x kilometrede benzinim bitti!" x değişkeni `kilometreSaati`ndeki sayı olmalıdır.
 */
 
-function Araba(/* kodlar buraya */) {
-  /* kodlar buraya */
+function Araba(model, yakitTuketimi) {
+  this.model = model;
+  this.benzinDeposu = 0;
+  this.kilometreSaati = 0;
+  this.yakitTuketimi = yakitTuketimi;
+  this.doldur = function (litre) {
+    this.benzinDeposu = litre + this.benzinDeposu;
+  };
+  this.kullan = function (mesafe) {
+    this.kilometreSaati = mesafe + this.kilometreSaati;
+    this.benzinDeposu = this.benzinDeposu - this.yakitTuketimi;
+    if (this.benzinDeposu == 0) {
+      return `${this.kilometreSaati}de benzinim bitti!`;
+    }
+  };
 }
 
 /*
   GÖREV 3
   Kendi cümlelerinle "this" kelimesinin 4 prensibini açıklayın:
-  1.
-  2.
-  3.
-  4.
+  1.Window Binding // this döndürecek hiçbir şeyi yokken global object olan window döner.
+
+  ///
+
+  2.Implicit Binding // 
+  const me = {
+    name: "Oguz"
+    age: 28
+    sayName = function(){
+      console.log(this.me)
+    }
+  }
+  me.sayName() --> this, fonksiyonun çağrıldığı yerde, noktanın soluna referans verir. bu durumda me.
+
+  ///
+
+  3.new Binding //
+  function Pet(name, breed, bark){
+    this.name = name;
+    this.breed = breed;
+    this.bark = bark;
+  }
+  Pet.prototype.sound = function(){ 
+    console.log(this.bark);
+  }
+
+  const ada = new Pet('Ada', 'Bali Dog', 'woof woof');
+  ada.sound()
+
+  4.Explicit Binding // Oluşturulan fonksiyonu this neye refere edilmek isteniyorsa. O noktada invoke ediyoruz.
+  Aşağıdaki örnekte this ada'dır.
+  function sound(){
+    console.log(this.bark);
+  }
+
+  const ada = {
+    name: 'Ada',
+    breed: 'Bali Dog',
+    bark: 'woof woof'
+  }
+
+sound.call(ada)
+    .call() 
+    .apply()
+    .bind()
+
 */
 
-
 /* !!!! Burdan aşağısını değiştirmeyin !!!! */
-function as(){
-  console.log('Kodlar sorunsuz çalışıyor!');
-  return 'sa';
+function as() {
+  console.log("Kodlar sorunsuz çalışıyor!");
+  return "sa";
 }
 as();
 module.exports = {
   as,
-    Kisi,
-    Araba
-/*'Bebek'*/
-}
+  Kisi,
+  Araba,
+  /*'Bebek'*/
+};
